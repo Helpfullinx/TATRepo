a88c29d562ff240d8b656ff7ae7cca200d46e10c
Matt Van Welzen <mattyvdub@gmail.com>
2021-11-04 23:45:21 -0500

    Added example scene with new Ground and Player sprites. Applied PlayerMove script to player.

--- TimeAfterTime/Assets/Scripts/PlayerMove.cs
+++ TimeAfterTime/Assets/Scripts/PlayerMove.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerMov : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
+    public Rigidbody2D rb;
     [SerializeField] float moveSpeedHor = 50f;
     [SerializeField] float moveSpeedVer = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float moveAmountHor = Input.GetAxis("Horizontal") * moveSpeedHor * Time.deltaTime;
         float moveAmountVer = Input.GetAxis("Vertical") * moveSpeedVer * Time.deltaTime;
