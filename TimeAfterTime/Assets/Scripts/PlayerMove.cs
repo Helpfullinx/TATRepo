fbb73e1e1bb537075dbe2fcdada4f4afa68be24e
Samuel <sammys.email.home@gmail.com>
2021-11-04 20:16:40 -0500

    Modified test script "PlayerMove"

--- TimeAfterTime/Assets/Scripts/PlayerMove.cs
+++ TimeAfterTime/Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMov : MonoBehaviour
 {
+    [SerializeField] float moveSpeedHor = 50f;
+    [SerializeField] float moveSpeedVer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class PlayerMov : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveAmountHor = Input.GetAxis("Horizontal") * moveSpeedHor * Time.deltaTime;
+        float moveAmountVer = Input.GetAxis("Vertical") * moveSpeedVer * Time.deltaTime;
+        transform.Translate(moveAmountHor, moveAmountVer, 0f);
     }
 }
