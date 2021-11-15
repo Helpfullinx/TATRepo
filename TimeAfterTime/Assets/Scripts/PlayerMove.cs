92ba00af16d0dd5175a8be5f0c8b559b357ec1cb
Zach van Welzen <zachvdub2002@gmail.com>
2021-11-15 09:36:20 -0600

    Ground Check Implemented

--- TimeAfterTime/Assets/Scripts/PlayerMove.cs
+++ TimeAfterTime/Assets/Scripts/PlayerMove.cs
@@ -13,36 +13,50 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float jumpHeight = 10f;
     [SerializeField] private float groundCheckDis = 1f;
     
-    public Rigidbody2D rb;
-    public CapsuleCollider2D _capsuleCollider2D;
+    private float moveAmountVer = 0f;
+    private float moveAmountHor = 0f;
+    private Rigidbody2D rb;
+    private CapsuleCollider2D _capsuleCollider2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Rigidbody2D>();
-        gameObject.GetComponent<CapsuleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
         LayerMask.GetMask("Ground");
     }
 
     private void Update()
     {
-        Debug.DrawRay(new Vector3(_capsuleCollider2D.bounds.center.x,_capsuleCollider2D.bounds.min.y),Vector3.down * ( _capsuleCollider2D.bounds.extents.y + groundCheckDis), Color.green);
+        Debug.DrawRay(new Vector3(_capsuleCollider2D.bounds.center.x,_capsuleCollider2D.bounds.min.y),Vector3.down * (groundCheckDis), Color.green);
+        /*
         Debug.Log(Grounded());
+        */
+        Debug.Log(Input.GetKey(KeyCode.Space));
+        
+        if (Grounded() & Input.GetKey(KeyCode.Space))
+        {
+            moveAmountVer = jumpHeight;
+        }
+        else
+        {
+            moveAmountVer = rb.velocity.y;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveAmountHor = Input.GetAxis("Horizontal") * moveSpeedHor * Time.deltaTime;
-        float moveAmountVer = Input.GetAxis("Jump") * jumpHeight * Time.deltaTime;
-        transform.Translate(moveAmountHor, moveAmountVer, 0f);
+        moveAmountHor = Input.GetAxis("Horizontal") * moveSpeedHor;
+        
+        rb.velocity = new Vector2(moveAmountHor, moveAmountVer);
     }
 
     bool Grounded()
     {
         LayerMask groundMask = LayerMask.GetMask("Ground");
         Vector2 playerBottom = new Vector2(_capsuleCollider2D.bounds.center.x,_capsuleCollider2D.bounds.min.y);
-        RaycastHit2D hit = Physics2D.Raycast(playerBottom,Vector2.down,_capsuleCollider2D.bounds.extents.y + groundCheckDis, groundMask);
+        RaycastHit2D hit = Physics2D.Raycast(playerBottom,Vector2.down, groundCheckDis, groundMask);
         return hit;
     }
 }
