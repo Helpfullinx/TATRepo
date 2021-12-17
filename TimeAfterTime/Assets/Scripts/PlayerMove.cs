5c9baa6088efd75c03fba19833b2ae939973a987
Zach van Welzen <zachvdub2002@gmail.com>
2021-12-17 00:40:26 -0600

    Cleaned Code

--- TimeAfterTime/Assets/Scripts/PlayerMove.cs
+++ TimeAfterTime/Assets/Scripts/PlayerMove.cs
@@ -1,5 +1,3 @@
-using Bolt;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
@@ -12,25 +10,23 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private Vector2 groundCheckCapsuleSize = new Vector2(1.1f,1.1f);
     [SerializeField] private GameObject cameraParent;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Facing facingDirection;
     
+
     [SerializeField] [Range(0f,5f)] private float cameraParentOffset = 5f;
     
     private Vector2 _moveAmount = Vector2.zero;
     private Rigidbody2D _rb;
-    private CapsuleCollider2D _capsuleCollider2D;
-    
     private Vector3 _rbPos;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _capsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
     //Update is called once per frame
     private void Update()
     {
-        
         Debug.Log(Grounded());
         
         if (Grounded() & Input.GetKey(KeyCode.Space))
@@ -51,13 +47,17 @@ public class PlayerMove : MonoBehaviour
 
         _rbPos = _rb.transform.position;
         
-        if (_rb.velocity.x < 0)
-        {
-            cameraParent.transform.position = new Vector3(_rbPos.x - cameraParentOffset, _rbPos.y,0);
-        }
-        else if (_rb.velocity.x > 0)
+        if (_rb.velocity.x < 0 && facingDirection != Facing.Left)
         {
             cameraParent.transform.position = new Vector3(_rbPos.x + cameraParentOffset, _rbPos.y,0);
+            facingDirection = Facing.Left;
+            transform.localScale = new Vector3(-1,1);
+        }
+        else if (_rb.velocity.x > 0 && facingDirection != Facing.Right)
+        {
+            cameraParent.transform.position = new Vector3(_rbPos.x - cameraParentOffset, _rbPos.y,0);
+            facingDirection = Facing.Right;
+            transform.localScale = new Vector3(1,1);
         }
     }
 
@@ -69,5 +69,11 @@ public class PlayerMove : MonoBehaviour
     {
         RaycastHit2D hit = Physics2D.CapsuleCast(_rb.position + groundCheckCenter, groundCheckCapsuleSize, CapsuleDirection2D.Vertical, -90, Vector2.down, 0, groundLayer);
         return hit.collider != null;
+    }
+    
+    private enum Facing
+    {
+        Left = -1,
+        Right = 1
     }
 }
